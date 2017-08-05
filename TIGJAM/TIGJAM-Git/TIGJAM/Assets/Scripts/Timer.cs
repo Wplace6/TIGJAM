43b4d5558702147e288eefdbdb590ad5cb032627
will place <will place>
2017-08-05 17:25:21 +1000

    Pushing changes to level test scene and timer script as well as adding jellyfish and scripts for post effects

--- TIGJAM/TIGJAM-Git/TIGJAM/Assets/Scripts/Timer.cs
+++ TIGJAM/TIGJAM-Git/TIGJAM/Assets/Scripts/Timer.cs
@@ -1,28 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
 
     [Tooltip("How long the game lasts, as a whole number")]
     public int breathLength = 45;
-
+    public GameObject BlackScreen;
+    public Color BlackScreenColor = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+    public Color ClearScreenColor = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+    public float fadetime;
+    public bool IsDead = false;
     public GameObject playerObj;
-    //public Vector3 startPosition = new Vector3(0,0,0);
-	
-	// Update is called once per frame
-	void Update ()
-    {
 
+    //public Vector3 startPosition = new Vector3(0,0,0);
+
+    // Update is called once per frame
+    void Start ()
+    {
+        Image img = BlackScreen.GetComponent<Image>();
+        img.color = ClearScreenColor;
+
+    }
+
+    void Update ()
+    {
+        
         if(playerObj.transform.position.x != 0 && playerObj.transform.position.z != 0)
         {
             StartCoroutine(Example());
         }
-		//if(player position != origin) {alarm[0] = 1 second}
+        //if(player position != origin) {alarm[0] = 1 second}
 
         //if(alarm[0]) {timeLength += ; reset alarm[0]}
 
         //if(breathLength <= timeLength){kill the player}
+
+        if (IsDead == true)
+        {
+            Image img = BlackScreen.GetComponent<Image>();
+            img.color = Color.Lerp(ClearScreenColor, BlackScreenColor, fadetime);
+            StartCoroutine(Restart());
+        }
 
 	}
 
@@ -32,5 +52,13 @@ public class Timer : MonoBehaviour {
         
         yield return new WaitForSeconds(breathLength);
         print("You're Dead");
+        IsDead = true;
+        
+    }
+
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(1);
+        Application.LoadLevel(Application.loadedLevel);
     }
 }
