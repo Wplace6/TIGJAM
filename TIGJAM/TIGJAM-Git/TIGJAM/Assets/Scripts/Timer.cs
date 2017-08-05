142bd99815118c1ca94a9cedfe799764cc4be9ff
Benjamin Turner <b3n.turner@gmail.com>
2017-08-05 10:37:17 +1000

    Added Death Timer n North,South,East,West Naming convention to cubes in scene

---
+++ TIGJAM/TIGJAM-Git/TIGJAM/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    [Tooltip("How long the game lasts, as a whole number")]
+    public int breathLength = 45;
+
+    public GameObject playerObj;
+    //public Vector3 startPosition = new Vector3(0,0,0);
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if(playerObj.transform.position.x != 0 && playerObj.transform.position.z != 0)
+        {
+            StartCoroutine(Example());
+        }
+		//if(player position != origin) {alarm[0] = 1 second}
+
+        //if(alarm[0]) {timeLength += ; reset alarm[0]}
+
+        //if(breathLength <= timeLength){kill the player}
+
+	}
+
+  
+    IEnumerator Example()
+    {
+        
+        yield return new WaitForSeconds(breathLength);
+        print("You're Dead");
+    }
+}
